e5f1efc683f1b202c76993e2e0dc6d367cbd11fd
htamagnus <agathamagnusg@hotmail.com>
2024-05-30 15:42:36 -0300

    🐛 fix: add tag playsInline to handle with background video in mobile

--- src/pages/InitialPage/InitialPage.tsx
+++ src/pages/InitialPage/InitialPage.tsx
@@ -6,14 +6,14 @@ import videoBackground from '../../assets/background.mp4';
 export default function InitialPage() {
     return (
         <div className={styles.container}>
-            <video className={styles.backgroundVideo} autoPlay loop muted>
+            <video className={styles.backgroundVideo} autoPlay loop muted playsInline>
                 <source src={videoBackground} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className={styles.content}>
                 <div className={styles.content__logo}>
                     <h1 className={styles.content__title}>ChronoMail</h1>
-                    <img src={hourglass} className={styles.content__image} alt="" />
+                    <img src={hourglass} className={styles.content__image} alt="hourglass" />
                 </div>
                 <div className={styles.texts}>
                     <p>Agende seus e-mails. Acerte o momento.</p>
