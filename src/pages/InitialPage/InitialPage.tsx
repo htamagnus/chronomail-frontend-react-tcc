dfe3bd2f01a23cb264cfa1cf3bc9910a1dbaccf3
htamagnus <agathamagnusg@hotmail.com>
2024-05-30 16:05:07 -0300

    🐛 create fallback to handle with background problems

--- src/pages/InitialPage/InitialPage.tsx
+++ src/pages/InitialPage/InitialPage.tsx
@@ -2,13 +2,14 @@ import styles from './InitialPage.module.scss';
 import hourglass from '../../assets/images/hourglass.png';
 import Form from '../../components/Form/FormEmail';
 import videoBackground from '../../assets/background.mp4';
+import backgroundFallback from '../../assets/images/background.png';
 
 export default function InitialPage() {
     return (
         <div className={styles.container}>
             <video className={styles.backgroundVideo} autoPlay loop muted playsInline>
                 <source src={videoBackground} type="video/mp4" />
-                Your browser does not support the video tag.
+                <img src={backgroundFallback} alt="Background Fallback" className={styles.backgroundImage} />
             </video>
             <div className={styles.content}>
                 <div className={styles.content__logo}>
