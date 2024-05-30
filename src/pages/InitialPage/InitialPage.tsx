e9027e11f6a089db4641f42fcea092921a1ca5d9
htamagnus <agathamagnusg@hotmail.com>
2024-05-30 16:12:16 -0300

    🐛 fix background fallback

--- src/pages/InitialPage/InitialPage.tsx
+++ src/pages/InitialPage/InitialPage.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import styles from './InitialPage.module.scss';
 import hourglass from '../../assets/images/hourglass.png';
 import Form from '../../components/Form/FormEmail';
@@ -5,12 +6,26 @@ import videoBackground from '../../assets/background.mp4';
 import backgroundFallback from '../../assets/images/background.png';
 
 export default function InitialPage() {
+    useEffect(() => {
+        const video = document.querySelector(`.${styles.backgroundVideo}`) as HTMLVideoElement | null;
+        const container = document.querySelector(`.${styles.container}`);
+        
+        if (video && container) {
+            video.onerror = () => {
+                container.classList.add(styles.videoFallback);
+            };
+        }
+    }, []);
+
     return (
         <div className={styles.container}>
-            <video className={styles.backgroundVideo} autoPlay loop muted playsInline>
-                <source src={videoBackground} type="video/mp4" />
+            <div className={styles.background}>
+                <video className={styles.backgroundVideo} autoPlay loop muted playsInline>
+                    <source src={videoBackground} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
                 <img src={backgroundFallback} alt="Background Fallback" className={styles.backgroundImage} />
-            </video>
+            </div>
             <div className={styles.content}>
                 <div className={styles.content__logo}>
                     <h1 className={styles.content__title}>ChronoMail</h1>
