f2c5130df920e7e882ebc1e385768cbbc5ece9c4
Ágatha <agathamagnusg@hotmail.com>
2024-06-01 16:17:20 -0300

    add env

--- src/providers/Api.ts
+++ src/providers/Api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'https://chronomail-backend-nestjs-tcc-production.up.railway.app';
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+if (!baseURL) {
+    throw new Error("The base URL for API is not defined in the environment variables");
+  }
 
 export const Api = axios.create({ baseURL });
